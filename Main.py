71b1da2b7f58350df1b0e7f9049d119907454b06
Ali <alastair.r.mcneill@gmail.com>
2021-01-09 12:05:06 +0000

    Added game loop

--- Main.py
+++ Main.py
@@ -1,3 +1,37 @@
-from minesweeper.Board import Board
+import pygame
+from minesweeper.Game import Game
+from minesweeper.Constants import WIN_HIEGHT, WIN_WIDTH, SQUARE_SIZE
 
-board = Board()
+WIN = pygame.display.set_mode((WIN_HIEGHT, WIN_WIDTH))
+pygame.display.set_caption("Minesweeper")
+CLOCK = pygame.time.Clock()
+
+def get_row_col_from_pos(pos):
+    row = pos[1] // SQUARE_SIZE
+    col = pos[0] // SQUARE_SIZE
+    return row, col
+
+
+def main():
+    run = True
+    game = Game()
+
+    while run:
+        CLOCK.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col =  get_row_col_from_pos(pos)
+                game.select(row, col)
+
+        game.draw(WIN)
+
+
+if __name__ == '__main__':
+    main()
