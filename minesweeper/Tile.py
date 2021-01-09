1282a7f0c1fa91df8f02a79382e02d917c9acac4
Ali <alastair.r.mcneill@gmail.com>
2021-01-09 12:06:46 +0000

    Changed drawing mechanics

--- minesweeper/Tile.py
+++ minesweeper/Tile.py
@@ -1,3 +1,6 @@
+from minesweeper.Constants import SQUARE_SIZE, WHITE, RED, BLUE, BOMB_IMG, TILE_IMG, NUM_IMGS
+import pygame
+
 class Tile:
     def __init__(self, row, col):
         self.row = row
@@ -7,13 +10,28 @@ class Tile:
         self.bomb = False
         self.shown = False
         self.surrounding_bombs = 0
-        self.get_pos()
+        self.flag = False
+        self.img = None
+        self.get_pos(row, col)
 
-    def get_pos(self):
-        pass
+    def get_pos(self, row, col):
+        self.x = col * SQUARE_SIZE
+        self.y = row * SQUARE_SIZE
+
+    def get_img(self):
+        if not self.shown:
+            self.img = TILE_IMG
+        else:
+            if self.bomb:
+                self.img = BOMB_IMG
+            else:
+                self.img = NUM_IMGS[self.surrounding_bombs]
+
+
 
     def set_bomb(self):
         self.bomb = True
 
-    def draw(self,win):
-        pass
+    def draw(self, win):
+        self.get_img()
+        win.blit(self.img, (self.x, self.y))
